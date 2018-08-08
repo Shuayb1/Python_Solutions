0ae0bd4c90d45d425f23d129430bbf42376298c3
Shuayb <shuaybpopoola@gmail.com>
2018-08-08 17:04:51 +0100

    Saving, with a raise

---
+++ ps0.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  6 13:08:17 2018
+
+@author: shuayb
+"""
+
+import numpy
+
+x = int(input("Enter number x: "))
+y = int(input("Enter number y: "))
+
+print("X**y = " + str(x**y))
+print("log(x) = " + str(numpy.log2(x)))
